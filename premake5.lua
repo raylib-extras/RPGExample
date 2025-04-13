1be6503871b68db743d5dcc57722c6cc280d630d
Jeffery Myers <jeffm2501@gmail.com>
2025-04-13 09:51:43 -0700

    Update to current game-premake

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,19 @@
+-- Copyright (c) 2020-2024 Jeffery Myers
+--
+--This software is provided "as-is", without any express or implied warranty. In no event 
+--will the authors be held liable for any damages arising from the use of this software.
+
+--Permission is granted to anyone to use this software for any purpose, including commercial 
+--applications, and to alter it and redistribute it freely, subject to the following restrictions:
+
+--  1. The origin of this software must not be misrepresented; you must not claim that you 
+--  wrote the original software. If you use this software in a product, an acknowledgment 
+--  in the product documentation would be appreciated but is not required.
+--
+--  2. Altered source versions must be plainly marked as such, and must not be misrepresented
+--  as being the original software.
+--
+--  3. This notice may not be removed or altered from any source distribution.
 
 newoption
 {
@@ -8,18 +24,26 @@ newoption
         { "opengl11", "OpenGL 1.1"},
         { "opengl21", "OpenGL 2.1"},
         { "opengl33", "OpenGL 3.3"},
-        { "opengl43", "OpenGL 4.3"}
+        { "opengl43", "OpenGL 4.3"},
+        { "opengles2", "OpenGLES 2.0"},
+        { "opengles3", "OpenGLES 3.0"}
     },
     default = "opengl33"
 }
 
-function define_C()
-    language "C"
-end
-
-function define_Cpp()
-    language "C++"
-end
+newoption
+{
+    trigger = "backend",
+    value = "BACKEND",
+    description = "backend to use",
+    allowed = {
+        { "GLFW", "GLFW"},
+        { "SDL2", "SDL2"},
+        { "SDL3", "SDL3"},
+        { "RLFW", "RLFW"}
+    },
+    default = "GLFW"
+}
 
 function string.starts(String,Start)
     return string.sub(String,1,string.len(Start))==Start
@@ -27,7 +51,8 @@ end
 
 function link_to(lib)
     links (lib)
-    includedirs ("../"..lib.."/include", "../"..lib.."/" )
+    includedirs ("../"..lib.."/include")
+    includedirs ("../"..lib.."/" )
 end
 
 function download_progress(total, current)
@@ -52,6 +77,61 @@ function check_raylib()
     end
 end
 
+function use_library(libraryName, githubFolder, repoHead)
+    libFolder = libraryName .. "-" .. repoHead
+    zipFile = libFolder .. ".zip"
+
+    baseName = path.getbasename(os.getcwd());
+
+    links(libraryName);
+    includedirs {"../" .. libFolder .. "/" }
+    includedirs {"../" .. libFolder .."/src/" }
+    includedirs {"../" .. libFolder .."/include/" }
+    
+
+    os.chdir("..")
+    
+    if(os.isdir(libFolder) == false) then
+        if(not os.isfile(zipFile)) then
+            print(libraryName .. " not found, downloading from github")
+            local result_str, response_code = http.download("https://github.com/" .. githubFolder .. "/archive/refs/heads/" .. repoHead ..".zip", zipFile, {
+                progress = download_progress,
+                headers = { "From: Premake", "Referer: Premake" }
+            })
+        end
+        print("Unzipping to " ..  os.getcwd())
+        zip.extract(zipFile, os.getcwd())
+        os.remove(zipFile)
+    end
+
+    os.chdir(libFolder)
+    
+    project (libraryName)
+        kind "StaticLib"
+        location "./"
+        targetdir "../bin/%{cfg.buildcfg}"
+
+        filter "action:vs*"
+            buildoptions { "/experimental:c11atomics" }
+
+        vpaths 
+        {
+            ["Header Files/*"] = { "include/**.h", "include/**.hpp",  "**.h", "**.hpp"},
+            ["Source Files/*"] = { "src/**.cpp", "src/**.c", "**.cpp",  "**.c"},
+        }
+        files {"include/**.hpp", "include/**.h","src/**.hpp", "src/**.h", "src/**.cpp", "src/**.c"}
+
+        includedirs { "./" }
+        includedirs { "./src" }
+        includedirs { "./include" }
+
+    os.chdir(baseName)
+end
+
+function use_Box2dV3()
+    use_library("box2d", "erincatto/box2d", "main")
+end
+
 workspaceName = path.getbasename(os.getcwd())
 
 if (string.lower(workspaceName) == "raylib") then
@@ -62,7 +142,9 @@ end
 
 workspace (workspaceName)
     configurations { "Debug", "Release"}
-    platforms { "x64", "x86"}
+    platforms { "x64", "x86", "ARM64"}
+
+    defaultplatform ("x64")
 
     filter "configurations:Debug"
         defines { "DEBUG" }
@@ -75,15 +157,19 @@ workspace (workspaceName)
     filter { "platforms:x64" }
         architecture "x86_64"
 
+    filter { "platforms:Arm64" }
+        architecture "ARM64"
+
     filter {}
 
-    targetdir "_bin/%{cfg.buildcfg}/"
+    targetdir "bin/%{cfg.buildcfg}/"
 
     if(os.isdir("game")) then
         startproject(workspaceName)
     end
 
-
+cdialect "C17"
+cppdialect "C++17"
 check_raylib();
 
 include ("raylib_premake5.lua")
@@ -94,7 +180,7 @@ end
 
 folders = os.matchdirs("*")
 for _, folderName in ipairs(folders) do
-    if (string.starts(folderName, "raylib") == false and string.starts(folderName, "_") == false and string.starts(folderName, ".") == false) then
+    if (string.starts(folderName, "raylib") == false and string.starts(folderName, ".") == false) then
         if (os.isfile(folderName .. "/premake5.lua")) then
             print(folderName)
             include (folderName)
--- raylib_premake5.lua
+++ raylib_premake5.lua
@@ -1,6 +1,32 @@
+-- Copyright (c) 2020-2024 Jeffery Myers
+--
+--This software is provided "as-is", without any express or implied warranty. In no event 
+--will the authors be held liable for any damages arising from the use of this software.
+
+--Permission is granted to anyone to use this software for any purpose, including commercial 
+--applications, and to alter it and redistribute it freely, subject to the following restrictions:
+
+--  1. The origin of this software must not be misrepresented; you must not claim that you 
+--  wrote the original software. If you use this software in a product, an acknowledgment 
+--  in the product documentation would be appreciated but is not required.
+--
+--  2. Altered source versions must be plainly marked as such, and must not be misrepresented
+--  as being the original software.
+--
+--  3. This notice may not be removed or altered from any source distribution.
 
 function platform_defines()
-    defines{"PLATFORM_DESKTOP"}
+     filter {"options:backend=GLFW"}
+        defines{"PLATFORM_DESKTOP"}
+
+    filter {"options:backend=RLFW"}
+        defines{"PLATFORM_DESKTOP_RGFW"}
+
+    filter {"options:backend=SDL2"}
+        defines{"PLATFORM_DESKTOP_SDL"}
+
+    filter {"options:backend=SDL3"}
+        defines{"PLATFORM_DESKTOP_SDL"}
 
     filter {"options:graphics=opengl43"}
         defines{"GRAPHICS_API_OPENGL_43"}
@@ -14,10 +40,17 @@ function platform_defines()
     filter {"options:graphics=opengl11"}
         defines{"GRAPHICS_API_OPENGL_11"}
 
+    filter {"options:graphics=openges3"}
+        defines{"GRAPHICS_API_OPENGL_ES3"}
+
+    filter {"options:graphics=openges2"}
+        defines{"GRAPHICS_API_OPENGL_ES2"}
+
     filter {"system:macosx"}
         disablewarnings {"deprecated-declarations"}
 
     filter {"system:linux"}
+        defines {"_GLFW_X11"}
         defines {"_GNU_SOURCE"}
 -- This is necessary, otherwise compilation will fail since
 -- there is no CLOCK_MONOTOMIC. raylib claims to have a workaround
@@ -49,23 +82,68 @@ function link_raylib()
     includedirs {"../" .. raylib_dir .."/src/external" }
     includedirs {"../" .. raylib_dir .."/src/external/glfw/include" }
     platform_defines()
+    
+    filter "files:**.dll"
+        buildaction "Copy"
 
     filter "action:vs*"
         defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS"}
         dependson {"raylib"}
         links {"raylib.lib"}
         characterset ("MBCS")
+        buildoptions { "/Zc:__cplusplus" }
 
     filter "system:windows"
         defines{"_WIN32"}
-        links {"winmm", "kernel32", "opengl32", "gdi32"}
-        libdirs {"../_bin/%{cfg.buildcfg}"}
+        links {"winmm", "gdi32", "opengl32"}
+        libdirs {"../bin/%{cfg.buildcfg}"}
 
     filter "system:linux"
-        links {"pthread", "GL", "m", "dl", "rt", "X11"}
+        links {"pthread", "m", "dl", "rt", "X11"}
 
     filter "system:macosx"
-        links {"OpenGL.framework", "Cocoa.framework", "IOKit.framework", "CoreFoundation.framework", "CoreAudio.framework", "CoreVideo.framework"}
+        links {"OpenGL.framework", "Cocoa.framework", "IOKit.framework", "CoreFoundation.framework", "CoreAudio.framework", "CoreVideo.framework", "AudioToolbox.framework"}
+
+    filter {"options:backend=SDL2"}
+        includedirs {"../SDL2/include" }
+
+    filter { "system:windows", "options:backend=SDL2", "platforms:x64"}
+        libdirs {"../SDL2/lib/x64"}
+        links {"SDL2"}
+        files "../SDL2/lib/x64/SDL2.dll"
+
+    filter { "system:windows", "options:backend=SDL2", "platforms:x32"}
+        libdirs {"../SDL2/lib/x32"}
+        links {"SDL2"}
+        files "../SDL2/lib/x32/SDL2.dll"
+
+    filter { "system:windows", "options:backend=SDL3", "platforms:x64", "action:vs*"}
+        includedirs {"../SDL3/include/SDL3" }
+        includedirs {"../SDL3/include" }
+        libdirs {"../SDL3/lib/x64"}
+        links {"SDL3"}
+        files "../SDL3/lib/x64/SDL3.dll"
+        
+    filter { "system:windows", "options:backend=SDL3", "platforms:x32", "action:vs*"}
+        includedirs {"../SDL3/include/SDL3" }
+        includedirs {"../SDL3/include" }
+        libdirs {"../SDL3/lib/x32"}
+        links {"SDL3"}
+        files "../SDL3/lib/x32/SDL3.dll"
+ 
+    filter { "system:windows", "options:backend=SDL3", "platforms:x64", "action:gmake*"}
+        includedirs {"../SDL3/x86_64-w64-mingw32/include/SDL3" }
+        includedirs {"../SDL3/x86_64-w64-mingw32/include" }
+        libdirs {"../SDL3/x86_64-w64-mingw32/lib/"}
+        libdirs {"../SDL3/x86_64-w64-mingw32/bin/"}
+        links {"SDL3"}
+        files "../SDL3/x86_64-w64-mingw32/bin/SDL3.dll"
+
+    filter { "system:*nix OR system:macosx", "options:backend=SDL2",  "configurations:Debug OR configurations:Release"}
+        links {"SDL2"}
+
+    filter { "system:*nix OR system:macosx", "options:backend=SDL3",  "configurations:Debug OR configurations:Release"}
+        links {"SDL3"}
 
     filter{}
 end
@@ -85,20 +163,31 @@ end
 
 project "raylib"
     kind "StaticLib"
+    raylib_dir = get_raylib_dir();
 
     platform_defines()
 
-    location "_build"
+    location (raylib_dir)
     language "C"
-    targetdir "_bin/%{cfg.buildcfg}"
+    targetdir "bin/%{cfg.buildcfg}"
+
+    filter {"options:backend=SDL2"}
+        includedirs {"SDL2/include" }
+
+     filter {"options:backend=SDL3", "action:vs*"}
+        includedirs {"SDL3/include/SDL3" }
+        includedirs {"SDL3/include" }
+        
+    filter { "system:windows", "options:backend=SDL3", "platforms:x64", "action:gmake*"}
+        includedirs {"SDL3/x86_64-w64-mingw32/include/SDL3" }
+        includedirs {"SDL3/x86_64-w64-mingw32/include" }
 
     filter "action:vs*"
         defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS"}
         characterset ("MBCS")
-
+        buildoptions { "/Zc:__cplusplus" }
     filter{}
 
-    raylib_dir = get_raylib_dir();
     print ("Using raylib dir " .. raylib_dir);
     includedirs {raylib_dir .. "/src", raylib_dir .. "/src/external/glfw/include" }
     vpaths
@@ -108,7 +197,7 @@ project "raylib"
     }
     files {raylib_dir .. "/src/*.h", raylib_dir .. "/src/*.c"}
 
-    removefiles {raylib_dir .. "/src/rcore_android.c", raylib_dir .. "/src/rcore_template.c", raylib_dir .. "/src/rcore_drm.c", raylib_dir .. "/src/rcore_web.c", raylib_dir .."/src/rcore_desktop.c"}
+    removefiles {raylib_dir .. "/src/rcore_*.c"}
 
     filter { "system:macosx", "files:" .. raylib_dir .. "/src/rglfw.c" }
         compileas "Objective-C"
